9777ee48a43e0d5b4f8adff6062add1fff5aedc0
Icaro Reis <icaroreis1982@gmail.com>
2025-11-19 20:10:15 -0300

    CustomNegotiation

---
+++ CustomNegotiation/src/main/java/br/com/icaro/paixao/config/WebConfig.java
@@ -0,0 +1,28 @@
+package br.com.icaro.paixao.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+//Conifgurações para utilizar CustomNegotiation no Projeto
+
+@Configuration
+public class WebConfig  implements WebMvcConfigurer {
+
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+
+
+        // VIA QUERY PARAM http://localhost:8080/api/test/V1/5?mediaType=xml
+        configurer.favorParameter(true)
+                .parameterName("mediaType")
+                .ignoreAcceptHeader(true)
+                .useRegisteredExtensionsOnly(false)
+                .defaultContentType(MediaType.APPLICATION_JSON)
+                .mediaType("json", MediaType.APPLICATION_JSON)
+                .mediaType("xml", MediaType.APPLICATION_XML);
+
+    }
+}
