7545978c6d7f4d3e9cdd1a86291598a424af09de
Icaro Reis <icaroreis1982@gmail.com>
2025-11-20 18:02:56 -0300

    HEADER PARAM

--- CustomNegotiation/src/main/java/br/com/icaro/paixao/config/WebConfig.java
+++ CustomNegotiation/src/main/java/br/com/icaro/paixao/config/WebConfig.java
@@ -14,11 +14,22 @@ public class WebConfig  implements WebMvcConfigurer {
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
 
-
+        /**
         // VIA QUERY PARAM http://localhost:8080/api/test/V1/5?mediaType=xml
         configurer.favorParameter(true)
                 .parameterName("mediaType")
                 .ignoreAcceptHeader(true)
+                .useRegisteredExtensionsOnly(false)
+                .defaultContentType(MediaType.APPLICATION_JSON)
+                .mediaType("json", MediaType.APPLICATION_JSON)
+                .mediaType("xml", MediaType.APPLICATION_XML);
+
+         */
+
+        // VIA HEADER PARAM http://localhost:8080/api/test/V1/5
+        // escolher o formato do dado pelo header do postman
+        configurer.favorParameter(false)
+                .ignoreAcceptHeader(false)
                 .useRegisteredExtensionsOnly(false)
                 .defaultContentType(MediaType.APPLICATION_JSON)
                 .mediaType("json", MediaType.APPLICATION_JSON)
